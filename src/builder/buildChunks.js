2c81fd2a644722472f35266143b312499a3981f5
Petr Vinichenko <p.vinichenko@omnidata.ru>
2025-11-03 17:42:50 +0300

    P-55

--- src/builder/buildChunks.js
+++ src/builder/buildChunks.js
@@ -26,6 +26,7 @@ const buildChunk = (chunk, params, ctx) => {
     let s = content;
     s = s.replace(/@:n\(.*"(&n)">@;/g, (a) => a.replace("&n", "\\small{&n}"));
     s = s.replace(/@:iso\(.*@;/g, (s)=>s.replace(`&v`, `\\color{gray}\\textit{&v}`));
+    s = s.replace(/@:fl\(.*@;/g, (s)=>s.replace(`&v`, `\\color{gray}\\textit{&v}`));
     return `<span class="echem-formula">${ChemSys.esc(s)}</span>`
   }
   if (type === "param") {
--- src/builder/buildTerm.js
+++ src/builder/buildTerm.js
@@ -11,6 +11,7 @@ const buildTerm = (code, dict) => {
     if (/[A-Z]/.test(code[0])) {
       term = term.slice(0, 1).toUpperCase() + term.slice(1);
     }
+    term = term.replace(/_([^_]*)_/g, "<i>$1</i>");
     return `<em class="term">${term}</em>`;
   }
   return `<span style="color: #F0C; font-weight: bold">${code}</span>`;
