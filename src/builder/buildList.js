e982e6954e38fc2849ee3a03988225aead2e05aa
Petr Vinichenko <p.vinichenko@omnidata.ru>
2025-11-30 23:04:50 +0300

    P-61.3.1

--- src/builder/buildList.js
+++ src/builder/buildList.js
@@ -2,20 +2,30 @@ const {drawTag} = require("charchem2/dist/utils/xml/drawTag");
 
 const buildList = (part, locale, ctx, buildPart) => {
   // Здесь используются теги ul/li. Хотя это не совсем соответствует стилистике книги, где нумерация списков такая: (a)
-  let cssType = "lower-latin";
-  let tag = "ul";
+  let cssType = undefined;
+  let htmlType = "a";
+
+  let tag = "ol";
   let start = undefined;
   if (/^\d+$/.test(part.nums)) {
-    cssType = "decimal";
-    tag = "ol";
+    // cssType = "decimal";
+    htmlType = "1";
     if (part.nums !== "1") start = part.nums;
+  } else if (/^[a-z]$/.test(part.nums)) {
+    if (part.nums !== "a") {
+      start = String(part.nums.charCodeAt(0) - "a".charCodeAt(0) + 1);
+    }
   } else if (part.nums === "*") {
+    tag = "ul"
     cssType = "disc";
   }
-  const attrs = {
-    style: `list-style: ${cssType};`,
-  };
-  if (start) attrs.start = start;
+  const attrs = {}
+  if (cssType) attrs.style = `list-style: ${cssType};`;
+  if (tag === "ol") {
+    attrs.type = htmlType;
+    if (start) attrs.start = start;
+  }
+  
   let res = drawTag(tag, attrs) + `\n`;
   part.items.forEach(item => {
     res += "<li>\n";
