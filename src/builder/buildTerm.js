bce963f2bdd2f00cb7b4909916bdd961c2eaf7b7
Petr Vinichenko <p.vinichenko@omnidata.ru>
2026-09-09 18:41:57 +0300

    P-68.1.2

--- src/builder/buildTerm.js
+++ src/builder/buildTerm.js
@@ -11,6 +11,10 @@ const buildTerm = (code, dict) => {
     if (/[A-Z]/.test(code[0])) {
       term = term.slice(0, 1).toUpperCase() + term.slice(1);
     }
+    if (/-[A-Z]/.exec(code)) {
+      // вариант, когда заглавная буква не первая, а после приставки с дефисом
+      term = term.replace(/-(.)/, (c) => c.toUpperCase())
+    }
     term = term.replace(/_([^_]*)_/g, "<i>$1</i>");
     return `<em class="term">${term}</em>`;
   }
